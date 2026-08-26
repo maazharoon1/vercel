b5736c398d19a1477b57f73e2aaf77961acbf0dd
maazharoon1 <maazharoon900@gmail.com>
2026-08-26 10:47:53 -0700

    up

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://spotonportfolio.example/sitemap.xml",
+    sitemap: "https://warsal-portfolio.com/sitemap.xml",
   };
 }
